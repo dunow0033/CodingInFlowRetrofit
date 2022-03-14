9502df3587dd5bfdeb762b2013d1fcb419e96a9f
Brian Donnoe <dunow33@hotmail.com>
2022-03-14 16:37:55 -0400

    initial commit

---
+++ app/src/main/java/com/bignerdranch/android/codinginflowretrofit/Post.java
@@ -0,0 +1,36 @@
+package com.bignerdranch.android.codinginflowretrofit;
+
+import com.google.gson.annotations.*;
+
+public class Post {
+    private int userId;
+
+    private Integer id;
+
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
